2851b4dfe13bd75066ccec9f83f6622f79790978
a2k-Oleksii <a2k_oleksii@ukr.net>
2020-11-20 14:15:15 +0200

    Add 3 methods in the class Calculate

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -2,4 +2,17 @@ public class Calculate {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int min(int a, int b) {
+        return a - b;
+    }
+
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    public double div(int a, int b) {
+        return a / b;
+    }
+
 }
